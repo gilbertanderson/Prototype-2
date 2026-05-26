9df386b786486c75ba75bcd0788b3e59dbb4f125
Gilbert Anderson <gilbertjanderson@gmail.com>
2026-05-26 22:13:02 +0300

    update script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
-public float horizontalInput;
-
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public float horizontalInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
