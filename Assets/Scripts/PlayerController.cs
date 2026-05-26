d2cb091fa83bd553ed90e82219fc0ae59407f4e4
Gilbert Anderson <gilbertjanderson@gmail.com>
2026-05-26 22:19:49 +0300

    update script for movement of player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,5 +15,6 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
